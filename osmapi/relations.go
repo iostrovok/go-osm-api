4800722e8d9398427c6d52920336b986e1b2eaee
Vlad Lashko <ostrovok@MacBook-Pro-Vladimir.local>
2014-10-31 15:18:12 +0300

    Relations. First commit.

---
+++ osmapi/relations.go
@@ -0,0 +1,315 @@
+package osmapi
+
+import (
+	"errors"
+	"time"
+)
+
+/*
+
+Relation create
+
+<?xml version="1.0" encoding="UTF-8"?>
+<osm version="0.6" generator="CGImap 0.3.3 (24667 thorn-03.openstreetmap.org)" \
+		copyright="OpenStreetMap and contributors" attribution="http://www.openstreetmap.org/copyright" \
+		license="http://opendatacommons.org/licenses/odbl/1-0/">
+ <relation id="2996185" visible="true" version="1" changeset="16527892" \
+ 	timestamp="2013-06-12T18:49:10Z" user="landfahrer" uid="1069176">
+  <member type="relation" ref="224645265" role="outer"/>
+  <member type="relation" ref="225456073" role="inner"/>
+  <tag k="type" v="multipolygon"/>
+ </relation>
+</osm>
+
+*/
+
+/* ===
+Relations in changesets
+*/
+
+type MemberSt struct {
+	Ref  string `xml:"ref,attr"`
+	Type string `xml:"type,attr"`
+	Role string `xml:"role,attr"`
+}
+
+type RelationSt struct {
+	Tags      []*TagSt    `xml:"tag"`
+	Members   []*MemberSt `xml:"member"`
+	OsmId     string      `xml:"id,attr"`
+	ReqId     string      `xml:"changeset,attr"`
+	Timestamp string      `xml:"timestamp,attr"`
+	Uid       string      `xml:"uid,attr,omitempty"`
+	User      string      `xml:"user,attr,omitempty"`
+	Version   string      `xml:"version,attr,omitempty"`
+	Visible   string      `xml:"visible,attr,omitempty"`
+	IsNew     bool        `xml:"-"`
+}
+
+/*
+When we want to modify or delete node we have get infomation from api.site
+*/
+func (ChSet *ChangeSetSt) RelationNew() (*RelationSt, error) {
+
+	r := RelationSt{}
+	r.Tags = nil
+	r.Members = nil
+	r.ReqId = ChSet.Id
+	r.Version = "1"
+	r.OsmId = "-1"
+	r.Visible = "true"
+	r.IsNew = true
+
+	tm := time.Now()
+	r.Timestamp = tm.Format(TimeFormatLayout)
+
+	ChSet.OsmCh._setRelation(&r)
+
+	return &r, nil
+}
+
+/*
+When we want to modify or delete node we have get infomation from api.site
+*/
+func (ChSet *ChangeSetSt) RelationLoad(OsmId string) (*RelationSt, error) {
+
+	/* Answer has to be empty */
+	data, err := ChSet.Request.GetXML("/api/0.6/relation/" + OsmId)
+	if err != nil {
+		return nil, err
+	}
+
+	r, err_r := ChSet.RelationNew()
+	if err_r != nil {
+		return nil, err_r
+	}
+	r.OsmId = OsmId
+	r.Version = xml_str(data, "/osm/relation/@version")
+	r.Visible = xml_str(data, "/osm/relation/@visible")
+	r.Uid = xml_str(data, "/osm/relation/@uid")
+	r.User = xml_str(data, "/osm/relation/@user")
+	r.IsNew = false
+
+	for _, v := range xml_slice(data, "/osm/relation/member", []string{"type", "ref", "role"}) {
+		if v["ref"] == "" {
+			continue
+		}
+		r._add_member(v["type"], v["ref"], v["role"])
+	}
+
+	for _, v := range xml_slice(data, "/osm/relation/tag", []string{"k", "v"}) {
+		r._add_tag(v["k"], v["v"])
+	}
+
+	return r, nil
+}
+
+func (ChSet *ChangeSetSt) RelationAddMember(t, ref, role string) error {
+
+	err := errors.New("RelationAddMember. Bad member type. Must be [way|node]. Now is " + t)
+	switch t {
+	case "node":
+		_, err = ChSet.LoadNodeDate(ref)
+	case "way":
+		_, err = ChSet.WayLoadData(ref)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	return ChSet._add_member(t, ref, role)
+}
+
+func (ChSet *ChangeSetSt) _add_member(t, ref, role string) error {
+	switch ChSet.OsmCh.Type {
+	case "modify":
+		return ChSet.OsmCh.Modify.Relation._add_member(t, ref, role)
+	case "create":
+		return ChSet.OsmCh.Create.Relation._add_member(t, ref, role)
+	case "delete":
+		return ChSet.OsmCh.Delete.Relation._add_member(t, ref, role)
+	}
+
+	return errors.New("_setRelation. No relation changeset")
+}
+
+func (r *RelationSt) _add_member(t, ref, role string) error {
+	if t == "" || ref == "" {
+		return errors.New("Relation. _add_member. Empty type or ref for member")
+	}
+	if r.Members == nil {
+		r.Members = []*MemberSt{}
+	}
+
+	m := MemberSt{}
+	m.Ref = ref
+	m.Type = t
+	m.Role = role
+	r.Members = append(r.Members, &m)
+
+	return nil
+}
+
+func (w *RelationSt) _add_tag(k, v string) {
+	if k == "" || v == "" {
+		return
+	}
+	if w.Tags == nil {
+		w.Tags = []*TagSt{}
+	}
+
+	t := TagSt{}
+	t.Key = k
+	t.Val = v
+
+	w.Tags = append(w.Tags, &t)
+}
+
+func (OsmCh *OsmChangeSt) _setRelation(relation *RelationSt) error {
+	switch OsmCh.Type {
+	case "modify":
+		OsmCh.Modify.Relation = relation
+	case "create":
+		OsmCh.Create.Relation = relation
+	case "delete":
+		OsmCh.Delete.Relation = relation
+	}
+	return errors.New("_setRelation. No relation changeset")
+
+}
+
+/*
+func (ChSet *ChangeSetSt) RelationAddNode(n *NodeSt, after_node_id ...string) (string, error) {
+	// Answer has to be empty
+	if !ChSet._checkRelation() {
+		//return "", errors.New("RelationAddNode. No relation changeset")
+	}
+
+	if n.OsmId == "" {
+		id, err := ChSet._next_ref_id()
+		if err != nil {
+			return "", err
+		}
+		n.OsmId = id
+	}
+
+	tm := time.Now()
+	n.Timestamp = tr.Format(TimeFormatLayout)
+
+	// TODO: Node already added into ChangeSet
+	// ChSet.OsmCh._addNode(n)
+	if err := ChSet._put_ref_to_relation(n.OsmId, after_node_id...); err != nil {
+		return "", err
+	}
+
+	ChSet._update_relation_id()
+
+	return n.OsmId, nil
+}
+
+
+func (ChSet *ChangeSetSt) RelationDelAllMembers() error {
+	// Answer has to be empty
+	if !ChSet._checkRelation() {
+		return errors.New("RelationDelNode. No relation changeset")
+	}
+
+	ChSet.DelAllNodes()
+	if err := ChSet._del_all_members_from_relation(); err != nil {
+		return err
+	}
+
+	return nil
+}
+*/
+/*
+func (ChSet *ChangeSetSt) RelationDelMember(OsmId string) error {
+	// Answer has to be empty
+	if !ChSet._checkRelation() {
+		return errors.New("RelationDelNode. No relation changeset")
+	}
+
+	ChSet.OsmCh.DelMember(OsmId)
+	//	if err := ChSet._del_ref_from_relation(OsmId); err != nil {
+	//		return err
+	//	}
+
+	return nil
+}
+*/
+/*
+func (ChSet *ChangeSetSt) _checkRelation() bool {
+	return ChSet.OsmCh._checkRelation()
+}
+*/
+/*
+func (OsmCh *OsmChangeSt) _checkRelation() bool {
+	switch OsmCh.Type {
+	case "modify":
+		if OsmCh.Modify.Relation != nil {
+			return true
+		}
+	case "create":
+		if OsmCh.Create.Relation != nil {
+			return true
+		}
+	case "delete":
+		if OsmCh.Delete.Relation != nil {
+			return true
+		}
+	}
+	return false
+}
+
+*/
+
+/*
+
+func (ChSet *ChangeSetSt) _del_all_ref_from_relation() error {
+	switch ChSet.OsmCh.Type {
+	case "modify":
+		ChSet.OsmCh.Modify.Relation = nil
+		return nil
+	case "create":
+		ChSet.OsmCh.Create.Relation = nil
+		return nil
+	case "delete":
+		ChSet.OsmCh.Delete.Relation = nil
+		return nil
+	}
+
+	return errors.New("_del_all_ref_from_relation. No relation changeset")
+}
+
+func (ChSet *ChangeSetSt) _del_ref_from_relation(ref string) error {
+	switch ChSet.OsmCh.Type {
+	case "modify":
+		return ChSet.OsmCh.Modify.Relation._del_ref_from_relation(ref)
+	case "create":
+		return ChSet.OsmCh.Create.Relation._del_ref_from_relation(ref)
+	case "delete":
+		return ChSet.OsmCh.Delete.Relation._del_ref_from_relation(ref)
+	}
+
+	return errors.New("_del_ref_from_relation. No relation changeset")
+}
+
+
+func (w *RelationSt) _del_ref_from_relation(NodeId string) error {
+
+	w._check_nodes()
+
+	nds := []*RelationNdSt{}
+	for _, v := range w.Nodes {
+
+		if v.Ref != NodeId {
+			nds = append(nds, v)
+		}
+	}
+
+	w.Nodes = nds
+	return nil
+}
+*/
+//======================
---
+++ osmapi/relations_test.go
@@ -0,0 +1,219 @@
+package osmapi
+
+import (
+	"github.com/davecgh/go-spew/spew"
+	"log"
+	"math/rand"
+	"os"
+	//"strconv"
+	"testing"
+)
+
+//export OSM_URL="http://api06.dev.openstreetmap.org"; export OSM_USER="lashko@corp.sputnik.ru"; export OSM_PASSWD="gdezhivetsputnik2";cd /kmsearch/go-osm-api/; set GOPATH="/kmsearch/go-osm-api/"; go test ./osmapi/relations.go ./osmapi/osmapi_relations_test.go ./osmapi/osmapi.go ./osmapi/changesets.go ./osmapi/capabilities.go
+
+const minimumVersion = "0.6"
+const maximumVersion = "0.6"
+
+var (
+	rnd            *rand.Rand = rand.New(rand.NewSource(99))
+	TestRelationId string     = ""
+)
+
+func Test_MyRequestSt(t *testing.T) {
+	req := MyRequest()
+	body, err := req.Get("")
+
+	if err != nil || body == "" {
+		log.Println("Test_MyRequestSt")
+		t.Fatal(err)
+	}
+}
+
+func init_req(t *testing.T, title string) *MyRequestSt {
+	login := os.Getenv("OSM_USER")
+	pass := os.Getenv("OSM_PASSWD")
+	url := os.Getenv("OSM_URL")
+
+	if login == "" || pass == "" {
+		log.Println("Skip " + title + ". login and password are not found.")
+		return nil
+	}
+
+	/*
+		if "CreateSetUpload" != title && "DeleteSetUpload" != title && "ChangeSetUpload" != title {
+			return nil
+		}
+	*/
+
+	req := MyRequest()
+	req.UserPass(login, pass)
+	if url == "" {
+		url = MainURLTest
+	}
+	req.SetUrl(url)
+
+	return req
+}
+
+func _ChangeSetClose(t *testing.T, c *ChangeSetSt) {
+	err := c.Close()
+	if err != nil {
+		log.Println("Test_ChangeSetCreate. Close.")
+		t.Fatal(err)
+	}
+}
+
+func Test_Relations(t *testing.T) {
+	//_01_RelationLoad(t)
+	_02_CreateRelations(t)
+	_03_ModifyRelations(t)
+	_04_DeleteRelations(t)
+}
+
+func _01_RelationLoad(t *testing.T) {
+
+	req := init_req(t, "CreateSetUpload")
+	if req == nil {
+		return
+	}
+
+	//req.SetDebug()
+
+	ChSet, err := req.Changesets("modify")
+	if err != nil {
+		log.Println("_01_RelationLoad. Changesets")
+		t.Fatal(err)
+	}
+
+	/* Load existing relation  2996187 */
+	if r, err_n := ChSet.RelationLoad("12993"); err_n != nil {
+		log.Println("_01_RelationLoad. RelationLoad")
+		t.Fatal(err_n)
+	} else {
+		spew.Dump(r)
+	}
+
+	_ChangeSetClose(t, ChSet)
+
+	//t.Fatal("test view")
+}
+
+func _02_CreateRelations(t *testing.T) {
+
+	req := init_req(t, "CreateSetUpload")
+	if req == nil {
+		return
+	}
+
+	//req.SetDebug()
+
+	ChSet, err := req.Changesets("create")
+	if err != nil {
+		log.Println("_02_CreateRelations. Create")
+		t.Fatal(err)
+	}
+
+	// Create new relation
+	if _, err := ChSet.RelationNew(); err != nil {
+		log.Println("_02_CreateRelations. RelationNew")
+		t.Fatal(err)
+	}
+
+	if err := ChSet.RelationAddMember("way", "52868", "outer"); err != nil {
+		log.Println("_02_CreateRelations. AddMember 52868")
+		t.Fatal(err)
+	}
+
+	if err := ChSet.RelationAddMember("node", "1282045", "inner"); err != nil {
+		log.Println("_02_CreateRelations. AddMember 1282045")
+		t.Fatal(err)
+	}
+
+	// Now our sequence is: -2 -1 -5 -4 -6
+	if TestRelationId, err = ChSet.Upload(); err != nil {
+		log.Println("_02_CreateRelations. Upload")
+		t.Fatal(err)
+	}
+
+	_ChangeSetClose(t, ChSet)
+
+	//t.Fatal("test view")
+}
+
+func _03_ModifyRelations(t *testing.T) {
+
+	req := init_req(t, "CreateSetUpload")
+	if req == nil {
+		return
+	}
+
+	req.SetDebug()
+
+	ChSet, err := req.Changesets("modify")
+	if err != nil {
+		log.Println("_03_ModifyRelations. Changesets")
+		t.Fatal(err)
+	}
+
+	// Load existing relation
+	if _, err_n := ChSet.RelationLoad(TestRelationId); err_n != nil {
+		log.Println("_04_DeleteRelations. RelationLoad")
+		t.Fatal(err_n)
+	}
+
+	if err := ChSet.RelationAddMember("way", "12820", "outer"); err != nil {
+		log.Println("_03_ModifyRelations. AddMember 52868")
+		t.Fatal(err)
+	}
+
+	if err := ChSet.RelationAddMember("node", "1282046", "inner"); err != nil {
+		log.Println("_03_ModifyRelations. AddMember 1282045")
+		t.Fatal(err)
+	}
+
+	// Now our sequence is: -2 -1 -5 -4 -6
+	if TestRelationId, err = ChSet.Upload(); err != nil {
+		log.Println("_03_ModifyRelations. Upload")
+		t.Fatal(err)
+	}
+
+	_ChangeSetClose(t, ChSet)
+
+	//t.Fatal("test view")
+}
+
+func _04_DeleteRelations(t *testing.T) {
+
+	if "" == TestRelationId {
+		t.Skip("_04_DeleteRelations")
+		return
+	}
+
+	req := init_req(t, "DeleteRelations")
+	if req == nil {
+		return
+	}
+
+	req.SetDebug()
+
+	ChSet, err := req.Changesets("delete")
+	if err != nil {
+		log.Println("_04_DeleteRelations. Create")
+		t.Fatal(err)
+	}
+
+	// Load existing relation
+	if _, err_n := ChSet.RelationLoad(TestRelationId); err_n != nil {
+		log.Println("_04_DeleteRelations. RelationLoad")
+		t.Fatal(err_n)
+	}
+
+	if TestRelationId, err = ChSet.Upload(); err != nil {
+		log.Println("_04_DeleteRelations. Upload")
+		t.Fatal(err)
+	}
+
+	_ChangeSetClose(t, ChSet)
+
+	//t.Fatal("test view")
+}
